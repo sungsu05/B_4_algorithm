b16add3834e537d9579d18b8bb12bf97f2744a9b
SungSuSon <thstjdtn05@gmail.com>
2023-05-01 09:34:57 +0900

    init

---
+++ 05_01/SonSungSu/solution_1.py
@@ -0,0 +1,11 @@
+# 문제 : 피자 나눠 먹기(2)
+# https://school.programmers.co.kr/learn/courses/30/lessons/120815?language=python3
+
+def solution(n):
+    for i in range(1,101):
+        if i*6 % n == 0:
+            return i
+
+print(solution(6))
+print(solution(10))
+print(solution(4))
