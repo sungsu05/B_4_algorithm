96ef8c30992edbdcda7e408112aaa3cfd21dbf40
Yi Yoonsung <lys11556633@gmail.com>
2023-05-01 23:59:48 +0900

    Add OOPtest
    권기현튜터님 특강 문제풀이입니다

---
+++ 05_01/Yoonsung/OOPtest/Shape.py
@@ -0,0 +1,24 @@
+"""
+Shape 클래스를 만들고
+    get_area 메서드를 갖게 해주세요.
+    
+Circle과 Rectangle 클래스를 만들때 Shape를 상속받아주세요
+    Circle은 Radius(반지름) 속성을 가지게
+    Rectangle(직사각형)은 length와 widht(폭) 속성을 가지게
+    get_area 메서드는 각각에 맞게 구현해주세요
+---
+    룰 : 10분 동안 명세만 보고 구현 고민하기.
+    막힌다면 5분 동안 복습, 타이핑은 no.
+    다시 고민 반복.
+"""
+class Shape:
+    def __init__(self):
+        pass
+    def get_area(self):
+        pass
+
+class Circle(Shape):
+    pass
+    
+class Rectangle(Shape):
+    pass
