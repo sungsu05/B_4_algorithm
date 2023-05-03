ebd665202be94e8623f566fb05ee3f1460dba9ab
SungSuSon <thstjdtn05@gmail.com>
2023-05-03 21:54:47 +0900

    :sparkles:Feat:update 05_03 solution

--- 05_03/SonSungSu/solution_3.py
+++ 05_03/SonSungSu/solution_3.py
@@ -17,38 +17,64 @@
 # print(x)
 
 # 오답 : 반례 1,1,2,3,1,2,3,1,2,3,1,2,3,1
-# def solution(ingredient):
-#     buger = "1231"
-#     ingredient = "".join(map(str,ingredient))
-#     result = 0
-#     while True:
-#         if buger in ingredient:
-#             print(ingredient)
-#             result += ingredient.count('1231')
-#             ingredient = ingredient.replace(buger,'')
-#             print(ingredient)
-#             continue
-#         break
-#     return result
 
+# 시간 초과
 def solution(ingredient):
+    ingredient = "".join(map(str,ingredient))
     result = 0
-    while True:
-        for i in range(len(ingredient)):
-            if ingredient[i:i+4] == [1,2,3,1]:
-                del ingredient[i:i+4]
-                result += 1
-                continue
-            break
-    return 0
+    while '1231' in ingredient:
+        result += 1
+        ingredient = ingredient.replace('1231','',1)
+    return result
 
-x = [2, 1, 1, 2, 3, 1, 2, 3, 1]
-print(solution(x))
-x = [1, 3, 2, 1, 2, 1, 3, 1, 2]
-print(solution(x))
+def solution_2(ingredient):
+    buger = '1231'
+    s = ''.join(list(map(str, ingredient)))
+    result = 0
+    while buger in s:
+        if s.find(buger) >= 4:
+            # print(s,s.find('1231'))
+            s = s[s.find(buger)-4:]
+            # print(s,s.find('1231'))
+        s = s.replace(buger,'',1)
+        result += 1
+    return result
+# 132121312222222222122221231231 , 22 -4
+# 22221231 , 4
+
+# -4를 하는 이유
+# 1, 3, 2, 1, 2, 1, 3, 1, 2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,1,1,2,3,1,2,3,1
+# 1, 3, 2, 1, 2, 1, 3, 1, 2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,1,1,2,3,1,2,3,1
+# 22211231231
+# '1231'을 잘라내고, 잘라낸 이전 값에서 새로운'1231'의 값이 생길 수 있다.
+
+
+# x = [2, 1, 1, 2, 3, 1, 2, 3, 1]
+# print(solution(x))
+# print(solution_2(x))
+
+x = [1, 3, 2, 1, 2, 1, 3, 1, 2,2,2,2,2,2,2,2,2,2,2,1,2,2,2,1,1,2,3,1,2,3,1]
+# print(solution(x))
+print(solution_2(x))
+
 x =[1,1,2,3,1,2,3,1,2,3,1,2,3,1]
-print(solution(x))
+# print(solution_2(x))
+# print(solution(x))
 
-x = [2, 1, 1, 2, 3, 1, 2, 3, 1]
-del x[0:0+4]
-print(x)
+
+
+
+
+
+# x = "012345123"
+# print(x.find('1'))
+# print(x.index('1'))
+# x = x.replace('123','',1)
+# print(x)
+
+# string = '712317771231888'
+# result = string.find('1231')
+# print(result)
+# s = string[string.find('1231')-4:] # 3-4 = -1
+#
+# print(s)
