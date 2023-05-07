03eff25af70b81244c69c6f4a7a9085db0ad5bb5
Bookwhale00 <Bookwhale00@gmail.com>
2023-05-07 23:11:39 +0900

    :sparkles: Feat: 5.5 solution 1,3
    2 하다가 막힘

--- 05_05/ParkYeongJu/solution_3.py
+++ 05_05/ParkYeongJu/solution_3.py
@@ -1,3 +1,15 @@
 '''
 3. https://www.acmicpc.net/problem/10172
+개
+이스케이프 문자를 적절하게 사용하기
+따옴표 3개 쓰기(multiline string 혹은 triple quotes라고 부른다.)
+* multiline string을 쓸 때 \ 를 쓰면 해당 줄만 줄바꿈을 하지 않음.
 '''
+
+print('''\
+|\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\\\__|
+''')
